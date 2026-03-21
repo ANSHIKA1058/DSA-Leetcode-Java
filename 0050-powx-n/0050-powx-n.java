2ede30c46a7de95e0f19722eb01ec08d4966af7f
ANSHIKA <anshika1058.becse24@chitkara.edu.in>
2026-03-21 21:04:24 +0530

    Time: 0 ms (100%), Space: 48.1 MB (44.14%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,19 @@
+class Solution {
+    public double myPow(double x, int n) {
+        int N=n;
+        if(N<0){
+            N=-N;
+            x=1/x;
+        }
+        return pow(x,N);
+    }
+    public double pow(double x,int n){
+        if(n==0) return 1;
+        double half= pow(x,n/2);
+        if(n%2==0){
+            return half*half;
+        }else{
+            return half*half*x;
+        }
+    }
+}
