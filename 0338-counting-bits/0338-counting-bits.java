c89d8c49bd4a929ae39dbabae90c92a51fa37225
ANSHIKA <anshika1058.becse24@chitkara.edu.in>
2026-03-22 13:52:47 +0530

    Time: 2 ms (96.64%), Space: 48.8 MB (44.66%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -2,7 +2,13 @@ class Solution {
     public int[] countBits(int n) {
         int arr[]= new int[n+1];
         for(int i=0;i<=n;i++){
-            arr[i]=Integer.bitCount(i);
+            int count=0;
+            int v=i;
+            while(v>0){
+                v=v&(v-1);
+                count++;
+            }
+            arr[i]=count;
         }
         return arr;
     }
