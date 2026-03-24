14a586bdd9ce33bc7a9dccc1245f7a4e60bece2b
ANSHIKA <anshika1058.becse24@chitkara.edu.in>
2026-03-24 09:50:09 +0530

    Time: 1 ms (95.17%), Space: 43.5 MB (65.33%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<List<Integer>> generate(int n) {
+        List<List<Integer>> result = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            List<Integer> row = new ArrayList<>();
+            for(int j=0;j<=i;j++){
+                if(j==0 || j==i){
+                    row.add(1);
+                }else{
+                    int sum = result.get(i-1).get(j-1)+result.get(i-1).get(j);
+                    row.add(sum);
+                }
+
+            }
+            result.add(row);
+        }
+        return result;
+    }
+}
