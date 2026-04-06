e7b84cb598b2d2c6bd713afe8d98f9f4d6e5afb3
ANSHIKA <anshika1058.becse24@chitkara.edu.in>
2026-04-06 17:23:28 +0530

    Time: 5 ms (16.07%), Space: 44.5 MB (95.95%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,23 +1,22 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        
-        int[] ans = new int[nums1.length];
-        Stack<Integer> st = new Stack<>();
-        HashMap<Integer,Integer> mp = new HashMap<>();
-        for(int i=nums2.length-1;i>=0;i--){
-            while(!st.isEmpty() && st.peek()<nums2[i]){
-                st.pop();
+        int[] arr = new int[nums1.length];
+        Arrays.fill(arr,-1);
+
+        for(int i=0;i<nums1.length;i++){
+             boolean flag = false;
+            for(int j=0;j<nums2.length;j++){
+                if(nums1[i]==nums2[j]){
+                    flag=true;
+                }
+                if(flag){
+                    if(nums1[i]<nums2[j]){
+                        arr[i]=nums2[j];
+                        break;
+                    }
+                }
             }
-            if(st.isEmpty()){
-                mp.put(nums2[i],-1);
-            }else{
-                mp.put(nums2[i],st.peek());
-            }
-            st.push(nums2[i]);
         }
-        for(int j=0;j<nums1.length;j++){
-             ans[j]=mp.get(nums1[j]);
-        }
-    return ans;
+        return arr;
     }
 }
