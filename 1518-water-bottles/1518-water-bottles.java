59c6d76174350985778a3226f72a33a755fe705b
ANSHIKA <anshika1058.becse24@chitkara.edu.in>
2026-04-08 14:50:48 +0530

    Time: 0 ms (100%), Space: 41.9 MB (90.91%) - LeetHub

--- 1518-water-bottles/1518-water-bottles.java
+++ 1518-water-bottles/1518-water-bottles.java
@@ -1,6 +1,7 @@
 class Solution {
     public int numWaterBottles(int n, int e) {
         int totaldrunk = n;
+        //tc =n linear sc=1 constant
         while(n>=e){
             n-=(e-1);
             totaldrunk++;
