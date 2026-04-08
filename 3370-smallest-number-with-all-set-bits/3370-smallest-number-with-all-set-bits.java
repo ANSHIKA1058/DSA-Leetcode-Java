7404f1efe21c13aedc79733c07d64e193056a099
ANSHIKA <anshika1058.becse24@chitkara.edu.in>
2026-04-08 14:17:55 +0530

    Time: 1 ms (38.7%), Space: 42.8 MB (10.49%) - LeetHub

--- 3370-smallest-number-with-all-set-bits/3370-smallest-number-with-all-set-bits.java
+++ 3370-smallest-number-with-all-set-bits/3370-smallest-number-with-all-set-bits.java
@@ -1,19 +1,6 @@
 class Solution {
     public int smallestNumber(int n) {
-        //phli approch 
-        //n=10
-        int temp = n;
-        //temp=10
-        int dig=0;
-        while(temp>0){
-            //10>0 dig=1 temp=5
-            //5>0 dig=2 temp=2
-            //2>0 dig =3 temp =1
-            //1>0 dig =4 temp =0
-            dig++;
-            temp>>=1;
-        }
-        //8-1=7 ans 
-        return (1<<dig)-1;
+        //dusri
+        return (1<<(Integer.toBinaryString(n).length()))-1;
     }
 }
