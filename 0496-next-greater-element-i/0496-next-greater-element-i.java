e0e1c9b3d4db09e503ddb8b12dd5405368543ced
ANSHIKA <anshika1058.becse24@chitkara.edu.in>
2026-04-09 13:18:10 +0530

    Time: 3 ms (92.83%), Space: 45.1 MB (71.12%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,25 +1,21 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int[] arr = new int[nums1.length];
-        for(int i=0;i<nums1.length;i++){
-            int j;
-            for(j=0;j<nums2.length;j++){
-                if(nums1[i]==nums2[j]){
+        HashMap<Integer,Integer> mp = new HashMap<>();
+
+        for(int i=0;i<nums2.length;i++){
+            int next=-1;
+            for(int j=i+1;j<nums2.length;j++){
+                if(nums2[j]>nums2[i]){
+                    next=nums2[j];
                     break;
                 }
             }
-
-                int next=-1;
-                for(int k=j+1;k<nums2.length;k++){
-                    if(nums2[k]>nums1[i]){
-                        next=nums2[k];
-                        break;
-                    }
-                }
-            arr[i]=next;
-            
-            
+            mp.put(nums2[i],next);
         }
-        return arr;
+        int[] ans = new int[nums1.length];
+        for(int i=0;i<nums1.length;i++){
+            ans[i]=mp.get(nums1[i]);
+        }
+        return ans;
     }
 }
