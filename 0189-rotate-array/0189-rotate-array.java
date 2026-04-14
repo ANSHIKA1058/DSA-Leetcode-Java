fda0eb4cbff530aeadd58108208bc34863854e4e
ANSHIKA <anshika1058.becse24@chitkara.edu.in>
2026-04-14 13:59:38 +0530

    Time: 0 ms (100%), Space: 61.4 MB (32.26%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -7,6 +7,7 @@ class Solution {
         reverse(nums,0,k-1);
         reverse(nums,k,n-1);
     }
+    // n tc 1 sc
     public void reverse(int[] arr , int i, int l){
         while(i<l){
             int temp=arr[i];
