28d91f77e276bdc4474cd8befc73d7a0cdc681f2
ANSHIKA <anshika1058.becse24@chitkara.edu.in>
2026-04-14 17:02:59 +0530

    Time: 25 ms (44.55%), Space: 48.7 MB (63.61%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,15 +1,18 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        //n3
-        int c=0;
+        //optimal refixsum+ map
         int n = nums.length;
-        for(int i=0;i<n;i++){
-            int sum=0;
-            for(int j=i;j<n;j++){
-                sum+=nums[j];
-                if(sum==k) c++;
+        int count=0;
+        HashMap<Integer,Integer> mp = new HashMap<>();
+        mp.put(0,1);
+        int sum=0;
+        for(int num:nums){
+            sum+=num;
+            if(mp.containsKey(sum-k)){
+                count+=mp.get(sum-k);
             }
+            mp.put(sum,mp.getOrDefault(sum,0)+1);
         }
-        return c;
+        return count;
     }
 }
