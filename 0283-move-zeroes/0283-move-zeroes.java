cfa09cfd696b1b7acfac217b3e95b15f180ba512
ANSHIKA <anshika1058.becse24@chitkara.edu.in>
2026-04-15 11:04:32 +0530

    Time: 2 ms (91.22%), Space: 47.7 MB (79.63%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,13 +1,13 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int k =0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]!=0){
-                nums[k++]=nums[i];
+        int l=0;
+        for(int r=0;r<nums.length;r++){
+            if(nums[r]!=0){
+                int temp = nums[r];
+                nums[r]=nums[l];
+                nums[l]=temp;
+                l++;
             }
-        }
-        while(k<nums.length){
-            nums[k++]=0;
         }
     }
 }
