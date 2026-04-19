a003665602fe1e54bb6019d352a535b72eb7beb2
ANSHIKA <anshika1058.becse24@chitkara.edu.in>
2026-04-19 18:33:54 +0530

    Time: 0 ms (100%), Space: 43.9 MB (19.69%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,23 +1,19 @@
 class Solution {
-    public void rotate(int[][] matrix) {
-        int r = matrix.length;
-        for(int i=0;i<r;i++){
-            for(int j =i;j<r;j++){
-                //transpose
-                int temp= matrix[i][j];
-                matrix[i][j]=matrix[j][i];
-                matrix[j][i]=temp;
+    public void rotate(int[][] mat) {
+        int n = mat.length;
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                int temp=mat[i][j];
+                mat[i][j]=mat[j][i];
+                mat[j][i]=temp;
             }
         }
-
-        //reverse each row;
-        for(int i=0;i<r;i++){
-            for(int j=0;j<r/2;j++){
-            int temp = matrix[i][j];
-            matrix[i][j]=matrix[i][r-j-1];
-            matrix[i][r-j-1]=temp;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n/2;j++){
+                int temp = mat[i][j];
+                mat[i][j]=mat[i][n-1-j];
+                mat[i][n-1-j]=temp;
+            }
         }
-        }
-        
     }
 }
