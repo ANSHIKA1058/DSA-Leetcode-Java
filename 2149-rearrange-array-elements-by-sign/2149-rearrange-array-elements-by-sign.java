5e8e4508adcfb1e88ffc45400e9457ed00eb0e3a
ANSHIKA <anshika1058.becse24@chitkara.edu.in>
2026-04-19 16:24:14 +0530

    Time: 5 ms (23.67%), Space: 172.4 MB (93.1%) - LeetHub

---
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] rearrangeArray(int[] nums) {
+        int n = nums.length;
+        int[] pos= new int[n/2];
+        int[] neg = new int[n/2];
+        int l=0;
+        int r=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]<0){
+                neg[l++]=nums[i];
+            }else{
+                pos[r++]=nums[i];
+            }
+        }
+        l=0;
+        r=0;
+        for(int i=0;i<n;i++){
+            if(i%2==0){
+                nums[i]=pos[l++];
+            }else{
+                nums[i]=neg[r++];
+            }
+        }
+        return nums;
+    }
+}
