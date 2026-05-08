2474994069227f6220d9e9bf224e01e4872ef0b8
ANSHIKA <anshika1058.becse24@chitkara.edu.in>
2026-05-08 08:38:49 +0530

    Time: 20 ms (12.16%), Space: 44.8 MB (95.5%) - LeetHub

---
+++ 0437-path-sum-iii/0437-path-sum-iii.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int pathSum(TreeNode root,long targetSum) {
+        if(root==null){
+            return 0;
+        }
+        return count(root,targetSum)+pathSum(root.left,targetSum)+pathSum(root.right,targetSum);
+    }
+    public int count(TreeNode root,long targetSum){
+        if(root==null){
+            return 0;
+        }
+        int ans=0;
+        if(root.val==targetSum){
+            ans++;
+        }
+        ans+=count(root.left,targetSum-root.val);
+        ans+=count(root.right,targetSum-root.val);
+        return ans;
+    }
+}
