9fb6857959ef845191ebc023dff8ca744766d7ee
ANSHIKA <anshika1058.becse24@chitkara.edu.in>
2026-05-08 08:45:47 +0530

    Time: 4 ms (50.07%), Space: 46.2 MB (50.76%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.java
+++ 0437-path-sum-iii/0437-path-sum-iii.java
@@ -14,22 +14,22 @@
  * }
  */
 class Solution {
-    public int pathSum(TreeNode root,long targetSum) {
-        if(root==null){
-            return 0;
-        }
-        return count(root,targetSum)+pathSum(root.left,targetSum)+pathSum(root.right,targetSum);
+    HashMap<Long,Integer> mp = new HashMap<>();
+    public int pathSum(TreeNode root, long targetSum) {
+        mp.put(0L,1);
+        return dfs(root,0L,targetSum);    
     }
-    public int count(TreeNode root,long targetSum){
+    public int dfs(TreeNode root,long currSum,long targetSum){
         if(root==null){
             return 0;
         }
-        int ans=0;
-        if(root.val==targetSum){
-            ans++;
-        }
-        ans+=count(root.left,targetSum-root.val);
-        ans+=count(root.right,targetSum-root.val);
-        return ans;
+        currSum+=root.val;
+        int count=mp.getOrDefault(currSum-targetSum,0);
+        mp.put(currSum,mp.getOrDefault(currSum,0)+1);
+        count+=dfs(root.left,currSum,targetSum);
+        count+=dfs(root.right,currSum,targetSum);
+
+        mp.put(currSum,mp.get(currSum)-1);
+        return count;
     }
 }
