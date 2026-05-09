24542f3ad4ef82b496c137e0426872e0f4c7074d
ANSHIKA <anshika1058.becse24@chitkara.edu.in>
2026-05-09 11:59:48 +0530

    Time: 0 ms (100%), Space: 44.1 MB (82.39%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -10,13 +10,15 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        ListNode prev = null;
-        ListNode curr= head;
-        ListNode next=null;
+        if(head==null || head.next==null){
+            return head;
+        }
+        ListNode curr=head;
+        ListNode prev=null;
         while(curr!=null){
-            next = curr.next;
+            ListNode next = curr.next;
             curr.next=prev;
-            prev = curr;
+            prev=curr;
             curr=next;
         }
         return prev;
