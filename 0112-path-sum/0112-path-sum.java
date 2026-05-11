f029a21c334ddcce5b4220109af3ff1540ee55d9
ANSHIKA <anshika1058.becse24@chitkara.edu.in>
2026-05-11 15:03:51 +0530

    Time: 1 ms (5.1%), Space: 45.3 MB (5.04%) - LeetHub

--- 0112-path-sum/0112-path-sum.java
+++ 0112-path-sum/0112-path-sum.java
@@ -14,15 +14,32 @@
  * }
  */
 class Solution {
-    public boolean hasPathSum(TreeNode root, int targ) {
-     if(root==null){
-        return false; 
-     }   
-     if((root!=null) && (root.left==null && root.right==null)){
-        if(root.val==targ){
-        return true;
-     }
-     }
-     return hasPathSum(root.left,targ-root.val)||hasPathSum(root.right,targ-root.val);
+    public boolean hasPathSum(TreeNode root, int t) {
+        List<Integer> list = new ArrayList<>();
+        return check(list,root,t);
     }
+    public boolean check(List<Integer> list,TreeNode root,int t){
+        if(root==null){
+            return false;
+        }
+        list.add(root.val);
+        if(root.left==null && root.right==null){
+            int sum=0;
+            for(int i=0;i<list.size();i++){
+                sum+=list.get(i);
+            }
+             list.remove(list.size() - 1);
+            if(sum==t){
+                return true;
+            }else{
+                return false;
+            }
+        }
+        boolean left =check(list,root.left,t);
+        boolean right=check(list,root.right,t);
+         list.remove(list.size() - 1);
+        return left||right; 
+
+    }
+
 }
