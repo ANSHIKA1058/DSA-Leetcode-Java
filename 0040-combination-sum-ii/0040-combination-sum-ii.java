eb4cc1f8023be747eeb42a7839659ba5b0b80b39
ANSHIKA <anshika1058.becse24@chitkara.edu.in>
2026-05-22 10:48:01 +0530

    Time: 6 ms (72.08%), Space: 48.1 MB (9.95%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,22 +1,21 @@
 class Solution {
-    public List<List<Integer>> combinationSum2(int[] arr, int target) {
+    public List<List<Integer>> combinationSum2(int[] cand, int t) {
         List<List<Integer>> ans = new ArrayList<>();
-        Arrays.sort(arr);
         List<Integer> list = new ArrayList<>();
-        helper(arr,target,0,ans,list);
+        Arrays.sort(cand);
+        helper(cand,t,list,ans,0);
         return ans;
     }
-    public void helper(int[] arr, int target, int idx,  List<List<Integer>> ans, List<Integer> list){
-        if(target==0){
+    public void helper(int[] cand,int t, List<Integer> list, List<List<Integer>> ans,int idx){
+        if(t==0){
             ans.add(new ArrayList<>(list));
             return;
         }
-
-        for(int i=idx;i<arr.length;i++){
-            if(i>idx && arr[i]==arr[i-1]) continue;
-            if(arr[i]>target) break;
-            list.add(arr[i]);
-            helper(arr, target-arr[i],i+1,ans, list);
+        for(int i=idx;i<cand.length;i++){
+            if(i>idx && cand[i]==cand[i-1]) continue;
+            if(cand[i]>t) break;
+            list.add(cand[i]);
+            helper(cand,t-cand[i],list,ans,i+1);
             list.remove(list.size()-1);
         }
     }
