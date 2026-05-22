ea1849a678baae4ed6ba9e4a904ebdf2396f6cce
ANSHIKA <anshika1058.becse24@chitkara.edu.in>
2026-05-22 10:26:05 +0530

    Time: 2 ms (94.91%), Space: 45.7 MB (66.58%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,23 +1,23 @@
 class Solution {
-    public List<List<Integer>> combinationSum(int[] cand, int target) {
+    public List<List<Integer>> combinationSum(int[] cand, int t) {
         List<List<Integer>> ans = new ArrayList<>();
         List<Integer> list = new ArrayList<>();
-        helper(cand,ans,list,0,target);
+        helper(cand,ans,list,t,0);
         return ans;
     }
-    public void helper(int[] cand, List<List<Integer>> ans , List<Integer> list, int idx , int target){
+    public void helper(int[] cand,List<List<Integer>> ans,List<Integer> list,int t,int idx){
         if(idx==cand.length){
-            if(target==0){
+            if(t==0){
                 ans.add(new ArrayList<>(list));
-            
             }
             return;
         }
-        if(cand[idx]<=target){
-        list.add(cand[idx]);
-        helper(cand,ans,list,idx,target-cand[idx]);
-        list.remove(list.size()-1);
+        if(cand[idx]<=t){
+            list.add(cand[idx]);
+            helper(cand,ans,list,t-cand[idx],idx);
+            list.remove(list.size()-1);
+
         }
-        helper(cand,ans,list,idx+1,target);
+        helper(cand,ans,list,t,idx+1);
     }
 }
