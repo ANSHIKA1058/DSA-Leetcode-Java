9b2c97674721f56f5671d9e336592e7bbea6b016
ANSHIKA <anshika1058.becse24@chitkara.edu.in>
2026-05-26 11:05:24 +0530

    Time: 3 ms (88.06%), Space: 43.1 MB (84.19%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,25 +1,29 @@
 class Solution {
     public boolean isValid(String s) {
+        int n =s.length();
         Stack<Character> st = new Stack<>();
-
-        for(int i=0;i<s.length();i++){
-            char ch = s.charAt(i);
-            if(ch=='(' || ch=='{' || ch=='[' ){
-                st.push(ch);
+        for(int i=0;i<n;i++){
+            char c = s.charAt(i);
+            if(c=='[' || c=='(' || c=='{'){
+                st.push(c);
             }else{
-                if(st.isEmpty()) return false;
-                char top = st.peek();
-                if((ch==')' && top=='(')||
-                (ch==']' && top=='[')||
-                (ch=='}' && top=='{')
-                ){
-                    st.pop();
-                }else {
-                return false;
+                if(st.isEmpty()){
+                    return false;
                 }
+            
+                    char t =st.peek();
+                    if((c==']' && t=='[')||
+                    (c=='}' && t=='{')||
+                    (c==')' && t=='(')){
+                        st.pop();
+                    }else{
+                        return false;
+                    }
+                }
+            
+
             }
-        }
+
         return st.isEmpty();
     }
-
 }
