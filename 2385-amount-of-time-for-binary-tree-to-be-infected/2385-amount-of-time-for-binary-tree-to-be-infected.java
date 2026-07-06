a9317798c575e95e9154dfe61a996a5cd172594c
ANSHIKA <anshika1058.becse24@chitkara.edu.in>
2026-07-06 17:58:50 +0530

    Time: 91 ms (67.75%), Space: 111.7 MB (64.78%) - LeetHub

--- 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.java
+++ 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.java
@@ -1,15 +1,14 @@
-
 class Solution {
-    HashMap<TreeNode,TreeNode> mp=new HashMap<>();
+    HashMap<TreeNode,TreeNode> mp = new HashMap<>();
     public int amountOfTime(TreeNode root, int start) {
-        TreeNode startnode= getStartNode(root,start);
-        Queue<TreeNode> q = new LinkedList<>();
+        TreeNode startNode = getStartNode(root,start);
+        Queue<TreeNode> q= new LinkedList<>();
         HashSet<TreeNode> vis = new HashSet<>();
-        q.add(startnode);
-        vis.add(startnode);
+        q.add(startNode);
+        vis.add(startNode);
         int t=-1;
         while(!q.isEmpty()){
-            int s=q.size();
+            int s= q.size();
             t++;
             TreeNode curr;
             for(int i=0;i<s;i++){
@@ -18,26 +17,32 @@ class Solution {
                     q.add(curr.left);
                     vis.add(curr.left);
                 }
-                if(curr.right!=null && !vis.contains(curr.right)){
+                 if(curr.right!=null && !vis.contains(curr.right)){
                     q.add(curr.right);
                     vis.add(curr.right);
                 }
+                /*
+                child - parent
+                10-3
+                6-3
+                3-1
+                */
                 if(mp.containsKey(curr) && !vis.contains(mp.get(curr))){
                     q.add(mp.get(curr));
                     vis.add(mp.get(curr));
-
                 }
 
             }
         }
         return t;
+
     }
     public TreeNode getStartNode(TreeNode root,int start){
-        TreeNode startnode=null;
+        TreeNode startnode = null;
         Queue<TreeNode> q = new LinkedList<>();
         q.add(root);
         while(!q.isEmpty()){
-            int s= q.size();
+            int s=q.size();
             TreeNode curr;
             for(int i=0;i<s;i++){
                 curr=q.poll();
@@ -48,13 +53,17 @@ class Solution {
                     q.add(curr.left);
                     mp.put(curr.left,curr);
                 }
-                 if(curr.right!=null){
+                if(curr.right!=null){
                     q.add(curr.right);
                     mp.put(curr.right,curr);
                 }
-
             }
+
         }
         return startnode;
+
+
+
+
     }
 }
