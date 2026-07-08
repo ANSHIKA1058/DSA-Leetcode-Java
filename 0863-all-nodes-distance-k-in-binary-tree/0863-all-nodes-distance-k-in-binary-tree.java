a96e0e5d05bbd9b1b3d31f6b8f2a7f3854d06f36
ANSHIKA <anshika1058.becse24@chitkara.edu.in>
2026-07-08 17:52:41 +0530

    Time: 12 ms (84.01%), Space: 43.8 MB (91.41%) - LeetHub

--- 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.java
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.java
@@ -1,62 +1,56 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
 class Solution {
-    public void markParents(TreeNode root,HashMap<TreeNode,TreeNode> parent){
+    public void makingparent(TreeNode root,HashMap<TreeNode,TreeNode> mp){
         Queue<TreeNode> q = new LinkedList<>();
         q.add(root);
+        TreeNode curr;
         while(!q.isEmpty()){
-            TreeNode curr=q.poll();
+            curr=q.poll();
             if(curr.left!=null){
                 q.add(curr.left);
-                parent.put(curr.left,curr);
+                mp.put(curr.left,curr);
             }
-            if(curr.right!=null){
+             if(curr.right!=null){
                 q.add(curr.right);
-                parent.put(curr.right,curr);
+                mp.put(curr.right,curr);
             }
-        }
+        }  
     }
     public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
-     HashMap<TreeNode,TreeNode> parent= new HashMap<>();
-     markParents(root,parent);
-     HashMap<TreeNode,Boolean> vis = new HashMap<>();
-     Queue<TreeNode> q = new LinkedList<>();
-     q.add(target);
-     vis.put(target,true);
-     int currLevel=0;
-     while(!q.isEmpty()){
-        int s= q.size();
-        if(currLevel==k) break;
-        currLevel++;
-        TreeNode curr;
-        for(int i=0;i<s;i++){
-            curr=q.poll();
-            if(curr.left!=null && vis.get(curr.left)==null){
-                q.add(curr.left);
-                vis.put(curr.left,true);
+        HashMap<TreeNode,TreeNode> parent = new HashMap<>();
+        makingparent(root,parent);
+        HashMap<TreeNode,Boolean> vis = new HashMap<>();
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(target);
+        vis.put(target,true);
+        int currlevel =0;
+        while(!q.isEmpty()){
+            int s= q.size();
+            if(currlevel==k) break;
+            currlevel++;
+            TreeNode curr;
+            for(int i=0;i<s;i++){
+                curr=q.poll();
+                if(curr.left!=null && vis.get(curr.left)==null){
+                    q.add(curr.left);
+                    vis.put(curr.left,true);
+                }
+                 if(curr.right!=null && vis.get(curr.right)==null){
+                    q.add(curr.right);
+                    vis.put(curr.right,true);
+                }
+                //5-3
+                if(parent.get(curr)!=null && vis.get(parent.get(curr))==null){
+                    q.add(parent.get(curr));
+                    vis.put(parent.get(curr),true);
+                }
             }
-             if(curr.right!=null && vis.get(curr.right)==null){
-                q.add(curr.right);
-                vis.put(curr.right,true);
-            }
-      if(parent.get(curr)!=null && vis.get(parent.get(curr))==null){
-        q.add(parent.get(curr));
-        vis.put(parent.get(curr),true);
-      }
-
         }
-     }
-     List<Integer> list = new ArrayList<>();
-     while(!q.isEmpty()){
-        list.add(q.poll().val);
-     }   
-     return list;
+        List<Integer> list = new ArrayList<>();
+        while(!q.isEmpty()){
+            list.add(q.poll().val);
+        }
+        return list;
+
+
     }
 }
