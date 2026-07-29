84dd8f726929e3530ee2ad2e056525af8265dee8
ANSHIKA <anshika1058.becse24@chitkara.edu.in>
2026-07-29 17:48:37 +0530

    Time: 8 ms (94.23%), Space: 47.5 MB (57.25%) - LeetHub

--- 0506-relative-ranks/0506-relative-ranks.java
+++ 0506-relative-ranks/0506-relative-ranks.java
@@ -1,20 +1,20 @@
 class Solution {
     public String[] findRelativeRanks(int[] score) {
         int n = score.length;
-        PriorityQueue<int[]> maxheap = new PriorityQueue<>((a,b) -> b[0]-a[0]);
-        for(int i=0;i<n;i++){
-            maxheap.offer(new int[]{score[i],i});
-        }
         String[] ans = new String[n];
+        PriorityQueue<int[]> pq= new PriorityQueue<>((a,b) -> b[0]-a[0]);
+        for(int i=0;i<n;i++){
+            pq.offer(new int[]{score[i],i});
+        }
         int rank=1;
-        while(!maxheap.isEmpty()){
-            int[] curr= maxheap.poll();
-            int idx =curr[1];
+        while(!pq.isEmpty()){
+            int[] curr = pq.poll();
+            int idx= curr[1];
             if(rank==1){
                 ans[idx]="Gold Medal";
             }else if(rank==2){
                 ans[idx]="Silver Medal";
-            }else if(rank==3){
+            }else if (rank==3){
                 ans[idx]="Bronze Medal";
             }else{
                 ans[idx]=String.valueOf(rank);
