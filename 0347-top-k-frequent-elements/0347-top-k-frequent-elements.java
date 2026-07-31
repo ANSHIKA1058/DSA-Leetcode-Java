e031764cb1d58871579cea50a0ce9c6cfc1d85de
ANSHIKA <anshika1058.becse24@chitkara.edu.in>
2026-07-31 17:24:22 +0530

    Time: 14 ms (70.85%), Space: 47.4 MB (82.95%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,30 +1,16 @@
 class Solution {
-    class Pair{
-        int num;
-        int freq;
-        Pair(int num,int freq){
-            this.num=num;
-            this.freq=freq;
-        }
-    }
     public int[] topKFrequent(int[] nums, int k) {
         HashMap<Integer,Integer> mp = new HashMap<>();
-        for(int n:nums){
-            mp.put(n,mp.getOrDefault(n,0)+1);
+        for(int i=0;i<nums.length;i++){
+            mp.put(nums[i],mp.getOrDefault(nums[i],0)+1);
         }
-        PriorityQueue<Pair> pq = new PriorityQueue<>(
-            (a, b) -> a.freq - b.freq
-        );
-        for(int ke :mp.keySet()){
-            pq.offer(new Pair(ke,mp.get(ke)));
-            if(pq.size()>k){
-                pq.poll();
-            }
+        PriorityQueue<int[]> maxheap = new PriorityQueue<>((a,b)->b[1]-a[1]);
+        for(int n:mp.keySet()){
+            maxheap.offer(new int[]{n,mp.get(n)});
         }
         int[] ans = new int[k];
-        int i=0;
-        while(!pq.isEmpty()){
-            ans[i++]=pq.poll().num;
+        for(int i=0;i<k;i++){
+            ans[i]=maxheap.poll()[0];
         }
         return ans;
     }
