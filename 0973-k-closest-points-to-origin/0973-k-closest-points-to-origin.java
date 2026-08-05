35f9836e236b0058a92e8b9d4877573815c2f405
ANSHIKA <anshika1058.becse24@chitkara.edu.in>
2026-08-05 13:55:21 +0530

    Time: 32 ms (36.46%), Space: 55.9 MB (34.65%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -1,17 +1,17 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
         PriorityQueue<int[]> maxheap = new PriorityQueue<>((a,b)->b[0]-a[0]);
-        for(int[] n:points){
-            int x = n[0];
-            int y = n[1];
-            int dis = x*x+y*y;
+        for(int[] p:points){
+            int x =p[0];
+            int y =p[1];
+            int dis= x*x+y*y;
             maxheap.offer(new int[]{dis,x,y});
             if(maxheap.size()>k){
                 maxheap.poll();
             }
         }
-        int[][] ans = new int[k][2];
         int i=0;
+        int[][] ans = new int[k][2];
         while(!maxheap.isEmpty()){
             int[] curr = maxheap.poll();
             ans[i][0]=curr[1];
@@ -20,5 +20,4 @@ class Solution {
         }
         return ans;
     }
-
 }
