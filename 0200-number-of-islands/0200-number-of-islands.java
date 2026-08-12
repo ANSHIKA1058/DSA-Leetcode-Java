4e43acc0b3bea3f45e3948d794b3a4088f6d9a38
ANSHIKA <anshika1058.becse24@chitkara.edu.in>
2026-08-12 19:14:09 +0530

    Time: 3 ms (87.56%), Space: 52.1 MB (72.95%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,62 +1,37 @@
 class Solution {
-    class Pair{
-        int row;
-        int col;
-        Pair(int row,int col){
-            this.row=row;
-            this.col=col;
-        }
-    }
     public int numIslands(char[][] grid) {
         int m = grid.length;
-        int n = grid[0].length;
-        int count=0;
+        int n = grid[0].length; 
         boolean[][] vis = new boolean[m][n];
+        int count=0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(!vis[i][j] && grid[i][j]=='1'){
+                if(grid[i][j]=='1' && !vis[i][j]){
+                    dfs(i,j,grid,vis);
                     count++;
-                    bfs(grid,vis,i,j);
                 }
             }
         }
         return count;
     }
-    public void bfs(char[][] grid,boolean[][] vis,int i, int j){
-        Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(i,j));
+
+    public void dfs(int i,int j ,char[][] grid, boolean[][] vis){
+        int m = grid.length;
+        int n = grid[0].length;
         vis[i][j]=true;
-        //no need to see left and top
-        while(q.size()>0){
-            Pair front = q.poll();
-            int row = front.row,col=front.col;
-            if(row>0){
-                if(vis[row-1][col]==false && grid[row-1][col]=='1'){
-                    q.add(new Pair(row-1,col));
-                    vis[row-1][col]=true;
-                }
-            }
+        if(i-1>=0 && grid[i-1][j]=='1' && !vis[i-1][j]){
+            dfs(i-1,j,grid,vis);
+        }
+        if(i+1<=m-1 && grid[i+1][j]=='1' && !vis[i+1][j]){
+            dfs(i+1,j,grid,vis);
+        }
 
-            if(row+1<grid.length){
-                if(vis[row+1][col]==false && grid[row+1][col]=='1'){
-                    q.add(new Pair(row+1,col));
-                    vis[row+1][col]=true;
-                }
-            }
+        if(j-1>=0 && grid[i][j-1]=='1' && !vis[i][j-1]){
+            dfs(i,j-1,grid,vis);
+        }
 
-            if(col+1<grid[0].length){
-                if(vis[row][col+1]==false && grid[row][col+1]=='1'){
-                    q.add(new Pair(row,col+1));
-                    vis[row][col+1]=true;
-                }
-            }
-
-            if(col>0){
-                if(vis[row][col-1]==false && grid[row][col-1]=='1'){
-                    q.add(new Pair(row,col-1));
-                    vis[row][col-1]=true;
-                }
-            }
+        if(j+1<=n-1 && grid[i][j+1]=='1' && !vis[i][j+1]){
+            dfs(i,j+1,grid,vis);
         }
     }
 }
