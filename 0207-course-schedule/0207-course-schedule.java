ae3bd51bd00f2ccd02ab1d04aeabf9d75971edf3
ANSHIKA <anshika1058.becse24@chitkara.edu.in>
2026-08-15 17:13:16 +0530

    Time: 4 ms (91.19%), Space: 46.9 MB (78.56%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,45 +1,39 @@
 class Solution {
+    static boolean ans;
     public boolean canFinish(int num, int[][] pre) {
-
+        ans =true;//no cycle=true
         List<List<Integer>> adj = new ArrayList<>();
-
+        
         for (int i = 0; i < num; i++) {
             adj.add(new ArrayList<>());
         }
 
-        int[] indegree = new int[num];
-
         for (int i = 0; i < pre.length; i++) {
             int a = pre[i][0];
             int b = pre[i][1];
-
             adj.get(b).add(a);
-            indegree[a]++;
         }
-
-        Queue<Integer> q = new LinkedList<>();
-
-        for (int i = 0; i < num; i++) {
-            if (indegree[i] == 0)
-                q.add(i);
-        }
-
-        int count = 0;
-
-        while (!q.isEmpty()) {
-
-            int front = q.poll();
-            count++;
-
-            for (int ele : adj.get(front)) {
-
-                indegree[ele]--;
-
-                if (indegree[ele] == 0)
-                    q.add(ele);
+        boolean[] vis = new boolean[num];
+        boolean[] path = new boolean[num];
+        for(int i=0;i<num;i++){
+            if(!vis[i]){
+                dfs(i,adj,vis,path);
             }
         }
+        return ans;
+        
+    }
+    public void dfs(int i,List<List<Integer>> adj, boolean[] vis,boolean[] path){
+        vis[i]=true;
+        path[i]=true;
 
-        return count == num;
+        for(int ele:adj.get(i)){
+            if(path[ele]==true){
+                ans=false;//fals means there is cycle
+                return;
+            }
+           if(!vis[ele]) dfs(ele,adj,vis,path);
+        }
+        path[i]=false;
     }
 }
