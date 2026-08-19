bf513502c39e142db03d4b7c3feffdce2df4cd76
ANSHIKA <anshika1058.becse24@chitkara.edu.in>
2026-08-19 17:57:26 +0530

    Time: 27 ms (44.21%), Space: 51.2 MB (79.74%) - LeetHub

--- 1386-cinema-seat-allocation/1386-cinema-seat-allocation.java
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.java
@@ -1,46 +1,48 @@
 class Solution {
     public int maxNumberOfFamilies(int n, int[][] reservedSeats) {
         HashMap<Integer,HashSet<Integer>> mp = new HashMap<>();
-        for(int[] arr:reservedSeats){
+        for(int[] arr: reservedSeats){
             int row = arr[0];
-            int val = arr[1];
+            int seats= arr[1];
             if(!mp.containsKey(row)){
                 mp.put(row,new HashSet<>());
             }
-            mp.get(row).add(val);
+            mp.get(row).add(seats);
         }
-
-        int ans = (n-mp.size())*2;
-
+        //non reserveds
+        int ans =(n-mp.size())*2;
         for(int r:mp.keySet()){
             HashSet<Integer> st = mp.get(r);
-            boolean l =true;
-            boolean m = true;
-            boolean ri= true;
-
+            //left 2 3 4 5
+            boolean l=true;
+            boolean m=true;
+            boolean right=true;
             for(int i=2;i<=5;i++){
                 if(st.contains(i)){
                     l=false;
                 }
             }
+
+            //middle 4 5 6 7
             for(int i=4;i<=7;i++){
                 if(st.contains(i)){
                     m=false;
                 }
             }
+
+            //right 6 7 8 9
             for(int i=6;i<=9;i++){
                 if(st.contains(i)){
-                    ri=false;
+                    right=false;
                 }
             }
 
-            if(l&&ri){
+            if(l && right){
                 ans+=2;
             }
-            else if(l || ri || m){
+            else if( l || m || right){
                 ans+=1;
             }
-
         }
         return ans;
     }
