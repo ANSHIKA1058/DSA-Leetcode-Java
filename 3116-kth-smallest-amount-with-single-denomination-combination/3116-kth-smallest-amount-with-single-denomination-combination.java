8239c27fe461ea1b767bcebc5deb240ebd697a3a
ANSHIKA <anshika1058.becse24@chitkara.edu.in>
2026-08-21 12:37:02 +0530

    Time: 197 ms (7.84%), Space: 43.3 MB (96.08%) - LeetHub

--- 3116-kth-smallest-amount-with-single-denomination-combination/3116-kth-smallest-amount-with-single-denomination-combination.java
+++ 3116-kth-smallest-amount-with-single-denomination-combination/3116-kth-smallest-amount-with-single-denomination-combination.java
@@ -1,51 +1,48 @@
 class Solution {
     public long findKthSmallest(int[] coins, int k) {
-        long low = 1, high = (long) coins[0] * k;
-
-        while (low < high) {
-            long mid = (low + high) / 2;
-
-            if (count(mid, coins) >= k)
-                high = mid;
-            else
-                low = mid + 1;
+        long low =1;
+        long high =(long) coins[0]*k;
+        while(low<high){
+            long mid= (low+high)/2;
+            if(count(mid,coins)>=k){
+                high=mid;
+            }else{
+                low=mid+1;
+            }
         }
-
         return low;
     }
-
-    long count(long x, int[] c) {
-        long ans = 0;
-        int n = c.length;
-
-        for (int mask = 1; mask < (1 << n); mask++) {
-            long lcm = 1;
-            int bits = 0;
-
-            for (int i = 0; i < n; i++) {
-                if ((mask & (1 << i)) != 0) {
-                    lcm = lcm(lcm, c[i]);
-                    if (lcm > x) break;
+    public long count(long x,int[] coins){
+        long ans =0;
+        int n = coins.length;
+        
+        for(int mask=1;mask<(1<<n);mask++){
+            long lcm =1;
+            int bits=0;
+            for(int i=0;i<n;i++){
+                if((mask  & (1<<i))!=0){
+                    lcm=lcm(lcm,coins[i]);
+                    if(lcm>x) break;
                     bits++;
                 }
             }
 
-            if (lcm <= x) {
-                if (bits % 2 == 1)
-                    ans += x / lcm;
-                else
-                    ans -= x / lcm;
+            if(lcm<=x){
+                if(bits%2==1){
+                    ans+=x/lcm;
+                }else{
+                    ans-=x/lcm;
+                }
             }
-        }
 
+            
+        }
         return ans;
     }
-
-    long gcd(long a, long b) {
-        return b == 0 ? a : gcd(b, a % b);
+    public long lcm(long a, long b){
+        return a/gcd(a,b)*b;
     }
-
-    long lcm(long a, long b) {
-        return a / gcd(a, b) * b;
+    public long gcd(long a, long b){
+        return b==0?a:gcd(b,a%b);
     }
 }
