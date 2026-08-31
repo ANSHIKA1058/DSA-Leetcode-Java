a8990c076c43856c10864158ae67926446754238
ANSHIKA <anshika1058.becse24@chitkara.edu.in>
2026-08-31 08:55:19 +0530

    Time: 57 ms (27.46%), Space: 47.7 MB (86.66%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
@@ -3,48 +3,40 @@ class Solution {
         int row;
         int col;
         int effort;
-        Triplet(int row, int col, int effort){
+        Triplet(int row,int col, int effort){
             this.row=row;
             this.col=col;
             this.effort=effort;
         }
         public int compareTo(Triplet t){
             if(this.effort==t.effort) return this.row-t.row;
-            return this.effort - t.effort;
+            return this.effort-t.effort;
         }
     }
+
+
     public int minimumEffortPath(int[][] arr) {
-        int m = arr.length;
-        int n = arr[0].length;
-        int[][] ans = new int[m][n];
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                ans[i][j]=Integer.MAX_VALUE;
-            }
+        int n = arr.length;
+        int m = arr[0].length;
+
+        int[][] ans = new int[n][m];
+      for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            ans[i][j]=Integer.MAX_VALUE;
         }
+      }
         ans[0][0]=0;
+
         PriorityQueue<Triplet> pq = new PriorityQueue<>();
         pq.add(new Triplet(0,0,0));
-        while(pq.size()>0){
+        while(!pq.isEmpty()){
             Triplet top = pq.remove();
             int row=top.row;
-            int col=top.col;
-            int effort=top.effort;
-
-            if(row==m-1 && col==n-1) break;
-
-            //top
-            if(row>0){
-                int e = Math.abs(arr[row][col]-arr[row-1][col]);
-                e=Math.max(e,effort);
-                if(e<ans[row-1][col]){
-                    ans[row-1][col]=e;
-                    pq.add(new Triplet(row-1,col,e));
-                }
-            }
-
+            int col = top.col;
+            int effort= top.effort;
+            if(row==n-1 && col==m-1) break;
             //left
-             if(col>0){
+            if(col>0){
                 int e = Math.abs(arr[row][col]-arr[row][col-1]);
                 e=Math.max(e,effort);
                 if(e<ans[row][col-1]){
@@ -52,23 +44,8 @@ class Solution {
                     pq.add(new Triplet(row,col-1,e));
                 }
             }
-
-            //bottom
-
-             if(row<m-1){
-                int e = Math.abs(arr[row][col]-arr[row+1][col]);
-                e=Math.max(e,effort);
-                if(e<ans[row+1][col]){
-                    ans[row+1][col]=e;
-                    pq.add(new Triplet(row+1,col,e));
-                }
-            }
-
-
-            // right
-
-
-             if(col<n-1){
+            //right
+             if(col<m-1){
                 int e = Math.abs(arr[row][col]-arr[row][col+1]);
                 e=Math.max(e,effort);
                 if(e<ans[row][col+1]){
@@ -76,8 +53,28 @@ class Solution {
                     pq.add(new Triplet(row,col+1,e));
                 }
             }
+            //top
+             if(row>0){
+                int e = Math.abs(arr[row][col]-arr[row-1][col]);
+                e=Math.max(e,effort);
+                if(e<ans[row-1][col]){
+                    ans[row-1][col]=e;
+                    pq.add(new Triplet(row-1,col,e));
+                }
+            }
+            //bottom
+             if(row<n-1){
+                int e = Math.abs(arr[row][col]-arr[row+1][col]);
+                e=Math.max(e,effort);
+                if(e<ans[row+1][col]){
+                    ans[row+1][col]=e;
+                    pq.add(new Triplet(row+1,col,e));
+                }
+            }
         }
-        return ans[m-1][n-1];
-
+        return ans[n-1][m-1];
     }
 }
+
+
+
