a1ba3a4089033eb075438288adb00154238e82d6
ANSHIKA <anshika1058.becse24@chitkara.edu.in>
2026-09-01 07:41:19 +0530

    Time: 24 ms (5.87%), Space: 48.3 MB (5.01%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -1,99 +1,74 @@
 class Solution {
 
-    class Pair {
+    class Pair{
         int node;
         int cost;
-
-        Pair(int node, int cost) {
+        Pair(int node, int cost){
             this.node = node;
             this.cost = cost;
         }
+
     }
 
-    class Triplet implements Comparable<Triplet> {
+    class Triplet implements Comparable<Triplet>{
         int node;
-        int cost;
+        int price;
         int stops;
-
-        Triplet(int node, int cost, int stops) {
-            this.node = node;
-            this.cost = cost;
-            this.stops = stops;
+        Triplet(int node, int price, int stops){
+            this.node=node;
+            this.price=price;
+            this.stops=stops;
         }
-
-        public int compareTo(Triplet t) {
-            if (this.stops == t.stops)
-                return this.cost - t.cost;
-
-            return this.stops - t.stops;
+        public int compareTo(Triplet t){
+            if(this.stops==t.stops) return this.price-t.price;
+            return this.stops-t.stops;
         }
     }
-
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-
-        List<List<Pair>> adj = new ArrayList<>();
-
-        for (int i = 0; i < n; i++) {
-            adj.add(new ArrayList<>());
+        List<List<Pair>> list = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            list.add(new ArrayList<>());
         }
-
-     
-        for (int i = 0; i < flights.length; i++) {
+        for(int i=0;i<flights.length;i++){
             int from = flights[i][0];
             int to = flights[i][1];
-            int price = flights[i][2];
+            int cost = flights[i][2];
 
-            adj.get(from).add(new Pair(to, price));
+            list.get(from).add(new Pair(to,cost));
         }
-
-        int[][] ans = new int[n][k + 2];
-
-        for (int i = 0; i < n; i++) {
-            Arrays.fill(ans[i], Integer.MAX_VALUE);
+        int[][] ans = new int[n][k+2];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<k+2;j++){
+                ans[i][j]=Integer.MAX_VALUE;
+            }
         }
+        ans[src][0]=0;
+        PriorityQueue<Triplet> pq= new PriorityQueue<>();
+        pq.add(new Triplet(src,0,0));//node price stops
 
-        ans[src][0] = 0;
-
-        PriorityQueue<Triplet> pq = new PriorityQueue<>();
-
-        pq.add(new Triplet(src, 0, 0));
-
-        while (!pq.isEmpty()) {
-
+        while(!pq.isEmpty()){
             Triplet top = pq.remove();
             int node = top.node;
-            int cost = top.cost;
-            int stops = top.stops;
-            if (stops == k + 1)
-                continue;
-
-            for (Pair p : adj.get(node)) {
-
-                int totalCost = cost + p.cost;
-
-                if (totalCost < ans[p.node][stops + 1]) {
-
-                    ans[p.node][stops + 1] = totalCost;
-
-                    pq.add(new Triplet(
-                        p.node,
-                        totalCost,
-                        stops + 1
-                    ));
+            int price = top.price;
+            int stops= top.stops;
+            if(stops==k+1) continue;
+            for(Pair p:list.get(node)){
+                int totalCost = price+p.cost;
+                if(totalCost<ans[p.node][stops+1]){
+                    ans[p.node][stops+1]=totalCost;
+                    pq.add(new Triplet(p.node,totalCost,stops+1));
                 }
             }
         }
-
-        
         int result = Integer.MAX_VALUE;
-
-        for (int stops = 0; stops <= k + 1; stops++) {
-            result = Math.min(result, ans[dst][stops]);
+        for(int stop=0;stop<=k+1;stop++){
+            result=Math.min(result,ans[dst][stop]);
         }
 
-        if (result == Integer.MAX_VALUE)
+        if(result==Integer.MAX_VALUE){
             return -1;
-
+        }
         return result;
+
     }
 }
