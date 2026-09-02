97b53fc4e96607445417cbaebc8cdac916b9f1d0
ANSHIKA <anshika1058.becse24@chitkara.edu.in>
2026-09-02 07:08:17 +0530

    Time: 12 ms (44.33%), Space: 48.8 MB (76.46%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.java
+++ 0743-network-delay-time/0743-network-delay-time.java
@@ -1,24 +1,26 @@
 class Solution {
     public int networkDelayTime(int[][] times, int n, int k) {
         int[] dist = new int[n+1];
-    Arrays.fill(dist,Integer.MAX_VALUE);
+        Arrays.fill(dist,Integer.MAX_VALUE);
         dist[k]=0;
+
         for(int x=1;x<=n-1;x++){
             for(int i=0;i<times.length;i++){
                 int u=times[i][0];
                 int v = times[i][1];
-                int d = times[i][2];
-                if(dist[u]!=Integer.MAX_VALUE && (dist[u]+d)<dist[v]){
-                    dist[v]=dist[u]+d;
+                int wt = times[i][2];
+//beelman
+                if(dist[u]+wt<dist[v] && dist[u]!=Integer.MAX_VALUE){
+                    dist[v]=dist[u]+wt;
                 }
             }
         }
-        int max =0;
-        for(int i=1;i<=n;i++){
+        int max=0;
+        for(int i=1;i<n+1;i++){
             if(dist[i]==Integer.MAX_VALUE) return -1;
-            max=Math.max(dist[i],max);
+            max=Math.max(max,dist[i]);
         }
-        
         return max;
+        
     }
 }
