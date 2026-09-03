adf3d3807e194d524e9013e6fe426be51b6191f3
ANSHIKA <anshika1058.becse24@chitkara.edu.in>
2026-09-03 07:36:18 +0530

    Time: 12 ms (36.41%), Space: 46.3 MB (85.48%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
@@ -1,40 +1,37 @@
 class Solution {
-    public int findTheCity(int n, int[][] edges, int threshold) {
+    public int findTheCity(int n, int[][] edges, int distanceThreshold) {
         int[][] dist = new int[n][n];
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
                 dist[i][j]=Integer.MAX_VALUE;
             }
         }
-
         for(int i=0;i<edges.length;i++){
-            int u = edges[i][0];
+            int u=edges[i][0];
             int v = edges[i][1];
             int wt = edges[i][2];
             dist[u][v]=wt;
             dist[v][u]=wt;
         }
-        //floys wolstl
+
         for(int k=0;k<n;k++){
             for(int i=0;i<n;i++){
                 if(i==k) continue;
                 for(int j=0;j<n;j++){
                     if(j==k) continue;
                     if(dist[i][k]!=Integer.MAX_VALUE && dist[k][j]!=Integer.MAX_VALUE){
-                    dist[i][j]=Math.min(dist[i][j],dist[i][k]+dist[k][j]);
-                }
+                        dist[i][j]=Math.min(dist[i][j],dist[i][k]+dist[k][j]);
+                    }
                 }
             }
         }
-
-
-        int mincity=-1;
-        int min=Integer.MAX_VALUE;
+        int mincity =-1;
+        int min =Integer.MAX_VALUE;
         for(int i=0;i<n;i++){
             int count=0;
             for(int j=0;j<n;j++){
                 if(i==j) continue;
-                if(dist[i][j]<=threshold) count++;
+                if(dist[i][j]<=distanceThreshold) count++;
             }
             if(count<=min){
                 min=count;
@@ -42,6 +39,7 @@ class Solution {
             }
         }
         return mincity;
-        
+
+
     }
 }
