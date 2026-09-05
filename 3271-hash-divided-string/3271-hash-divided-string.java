636b53b6314c85f41aa2378a08a6c358336b2b1a
ANSHIKA <anshika1058.becse24@chitkara.edu.in>
2026-09-05 20:34:49 +0530

    Time: 2 ms (97.37%), Space: 46.3 MB (78.95%) - LeetHub

--- 3271-hash-divided-string/3271-hash-divided-string.java
+++ 3271-hash-divided-string/3271-hash-divided-string.java
@@ -1,18 +1,17 @@
 class Solution {
     public String stringHash(String s, int k) {
-        int i=0;
         int n =s.length();
+        int i=0;
         StringBuilder sb = new StringBuilder();
         while(i<n){
             int sum=0;
             for(int j=i;j<i+k;j++){
                 sum+=(s.charAt(j)-'a');
-
             }
-            char c = (char)('a'+(sum%26));
-            sb.append(c);
+            char ch = (char)('a'+(sum%26));
+            sb.append(ch);
             i+=k;
-        }
-        return sb.toString();
+        } 
+        return sb.toString();    
     }
 }
