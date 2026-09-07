3bdaf2fe6c5bee1aaa55bb69885daf7864ae9243
ANSHIKA <anshika1058.becse24@chitkara.edu.in>
2026-09-07 21:04:05 +0530

    Time: 2 ms (41.53%), Space: 47.3 MB (58.95%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,29 +1,39 @@
 class Solution {
-    public void bfs(int[][] isConnected,boolean[] isVis,int i){
-        Queue<Integer> q = new LinkedList<>();
-        q.add(i);
-        isVis[i]=true;
-        while(!q.isEmpty()){
-            int p =q.poll();
-        for(int j=0;j<isConnected.length;j++){
-            
-            if(isConnected[p][j]==1 && !isVis[j]){
-                q.add(j);
-                isVis[j]=true;
-            }
-        }
-        }
-    }
+    static int[] parent;
     public int findCircleNum(int[][] isConnected) {
         int n = isConnected.length;
-        boolean[] isVis= new boolean[n];
-        int count=0;
+        parent=new int[n+1];
+        for(int i=1;i<=n;i++){
+            parent[i]=i;
+        }
+        
+
         for(int i=0;i<n;i++){
-            if(!isVis[i]){
-                count++;
-                bfs(isConnected,isVis,i);
+            for(int j=0;j<n;j++){
+                if(i==j) continue;
+                if(i!=j && isConnected[i][j]==1) union(i+1,j+1);
             }
         }
+
+        int count=0;
+        for(int i=1;i<=n;i++){
+            if(parent[i]==i) count++;
+        }
         return count;
+    }
+
+
+    public int find(int a){
+        if(parent[a]==a) return a;
+        return find(parent[a]);
+    }
+
+    public void union(int a, int b){
+        int leaderA=find(a);
+        int leaderB=find(b);
+
+        if(leaderA!=leaderB){
+            parent[leaderB]=leaderA;
+        }
     }
 }
