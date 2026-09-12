d570881edd829f4a62fa1c55e030423dfa5b696f
ANSHIKA <anshika1058.becse24@chitkara.edu.in>
2026-09-12 10:50:42 +0530

    Time: 0 ms (100%), Space: 46.6 MB (86.72%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -14,16 +14,15 @@ public class Solution {
         if(head==null || head.next==null){
             return false;
         }
-       int count=0;
         ListNode slow=head;
-        ListNode fast=head;
+        ListNode fast= head;
         while(fast!=null && fast.next!=null){
             slow=slow.next;
             fast=fast.next.next;
-            if(fast==slow){
+            if(slow==fast){
                 return true;
             }
         }
-      return false;
+        return false;
     }
 }
