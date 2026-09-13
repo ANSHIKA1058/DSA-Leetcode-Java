fcc786784a4386334b984dda6cf1079dda01c2ed
ANSHIKA <anshika1058.becse24@chitkara.edu.in>
2026-09-13 10:04:38 +0530

    Time: 0 ms (100%), Space: 46.9 MB (13.62%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -11,28 +11,24 @@
  */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-        if (head == null || head.next == null) {
+        if(head==null || head.next==null){
             return null;
         }
-
-        ListNode slow = head;
+        ListNode slow=head;
         ListNode fast = head;
-
-        while (fast.next!= null && fast.next.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-
-            if (fast == slow) {
+        while(fast.next!=null && fast.next.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast){
                 ListNode temp = head;
-                while (temp != slow) {
-                    temp = temp.next;
-                    slow = slow.next;
+                while(temp!=slow){
+                    temp=temp.next;
+                    slow=slow.next;
                 }
                 return slow;
             }
         }
-
-
         return null;
+        
     }
 }
