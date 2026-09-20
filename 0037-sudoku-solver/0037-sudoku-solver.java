9365a1f1b448cd2f2e262d6104b9539a2bd81c97
ANSHIKA <anshika1058.becse24@chitkara.edu.in>
2026-09-20 12:55:10 +0530

    Time: 677 ms (5.17%), Space: 43 MB (43.39%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -1,6 +1,16 @@
 class Solution {
-    public void solve(char[][] board, int row , int col, char[][] grid){
-        if(row==9){
+    public void solveSudoku(char[][] board) {
+        char[][] grid = new char[9][9];
+        solve(board,grid,0,0);
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                board[i][j]=grid[i][j];
+            }
+        }
+    }
+
+    public void solve(char[][] board, char[][] grid, int r, int c){
+        if(r==9){
             for(int i=0;i<9;i++){
                 for(int j=0;j<9;j++){
                     grid[i][j]=board[i][j];
@@ -8,69 +18,49 @@ class Solution {
             }
             return;
         }
-
-
-        if(board[row][col]!='.'){
-        if(col!=8){
-            solve(board,row,col+1,grid);
+        if(board[r][c]!='.'){
+            if(c!=8){
+                solve(board,grid,r,c+1);
+            }else{
+                solve(board,grid,r+1,0);
+            }
         }else{
-            solve(board,row+1,0,grid);
-        }
-        }
 
-
-
-        else{
             for(char ch='1';ch<='9';ch++){
-                if(isValid(board,row,col,ch)){
-                    board[row][col]=ch;
-            if(col!=8) solve(board,row,col+1,grid);
-            else solve(board,row+1,0,grid);
-            board[row][col]='.';
+                if(isValid(ch,board,r,c)){
+                    board[r][c]=ch;
+                    if(c!=8) solve(board,grid,r,c+1);
+                    else solve(board,grid,r+1,0);
+                    board[r][c]='.';
                 }
-                }
+            }
         }
-
-
-         
     }
 
-    private boolean isValid(char[][] board, int row, int col, char num){
-        //row check
-      
-        for(int j=0;j<9;j++){
-            if(board[row][j]==num) return false;
-        }
-
-        //col check
+    public boolean isValid(char ch, char[][] board, int r, int c){
+        //row
         for(int i=0;i<9;i++){
-            if(board[i][col]==num) return false;
+            if(board[r][i]==ch) return false;
         }
 
-        //check 3*3 ki grid
-        int i=0;
-        int j=0;
-        i=row/3;
-        j=col/3;
+        //col
+        for(int j =0;j<9;j++){
+            if(board[j][c]==ch) return false;
+        }
+
+        //3*3
+       
+        int i=r/3;
+        int j=c/3;
         i*=3;
         j*=3;
-        for(int k = i;k<i+3;k++){
-            for(int l=j;l<j+3;l++){
-                if(board[k][l]==num) return false;
+        for(int row=i;row<i+3;row++){
+            for(int col=j;col<j+3;col++){
+            if(board[row][col]==ch){
+                return false;
+            }
             }
         }
         return true;
-
-    }
-
-
-    public void solveSudoku(char[][] board) {
-        char[][] grid = new char[9][9];
-        solve(board,0 ,0,grid);
-        for(int i=0;i<9;i++){
-            for(int j=0;j<9;j++){
-                board[i][j]=grid[i][j];
-            }
-        }
     }
 }
